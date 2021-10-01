75e61782b646fd7a90376a5b66c0124acd58d2bd
chobitsfan <chobits@itri.org.tw>
2021-10-01 16:19:13 +0800

    different beam color

--- Assets/VirtualAction.cs
+++ Assets/VirtualAction.cs
@@ -10,9 +10,31 @@ public class VirtualAction : MonoBehaviour
     public bool isPlayer;
     //int hp = 3;
     float hitVibCd = 0f;
+    Color beamColor = Color.green;
+
+    private void Start()
+    {
+        int r = 200, g = 10, b = 10;
+        string f_name;
+        if (isPlayer) f_name = "player_beam_color.txt"; else f_name = "emery_beam_color.txt";
+        try
+        {
+            string[] color_txt = System.IO.File.ReadAllText(f_name).Split(',');
+            r = int.Parse(color_txt[0]);
+            g = int.Parse(color_txt[1]);
+            b = int.Parse(color_txt[2]);
+        }
+        catch (System.Exception)
+        {
+
+        }
+        beamColor = new Color(r / 255.0f, g / 255.0f, b / 255.0f);
+    }
+
     public void Shot()
     {
         var beam = GameObject.Instantiate(beamShot, transform.position + transform.forward * 0.1f, Quaternion.LookRotation(-transform.right));
+        beam.GetComponent<VolumetricLines.VolumetricLineBehavior>().LineColor = beamColor;
     }
 
     IEnumerator SmokeLater()
