acbc196208f9a82ec5985228b9e776dc17c0dd1f
chobitsfan <chobits@itri.org.tw>
2021-10-07 15:30:11 +0800

    fix  warnning

--- Assets/EffectExamples/Misc Effects/Scripts/SpawnEffect.cs
+++ Assets/EffectExamples/Misc Effects/Scripts/SpawnEffect.cs
@@ -9,46 +9,21 @@ public class SpawnEffect : MonoBehaviour {
     public AnimationCurve fadeIn;
 
     ParticleSystem ps;
-    float timer = 0;
-    Renderer _renderer;
 
     int shaderProperty;
 
 	void Start ()
     {
         shaderProperty = Shader.PropertyToID("_cutoff");
-        _renderer = GetComponent<Renderer>();
         ps = GetComponentInChildren <ParticleSystem>();
 
         var main = ps.main;
         main.duration = spawnEffectTime;
 
-        //ps.Play();
-
     }
 	
-	void Update ()
-    {
-        /*if (timer < spawnEffectTime + pause)
-        {
-            timer += Time.deltaTime;
-        }
-        else
-        {
-            ps.Play();
-            timer = 0;
-        }*/
-        if (timer < spawnEffectTime)
-        {
-            timer += Time.deltaTime;
-            _renderer.material.SetFloat(shaderProperty, fadeIn.Evaluate(Mathf.InverseLerp(0, spawnEffectTime, timer)));
-        }
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         ps.Play();
-        timer = 0;
     }
 }
--- Assets/UnityChan/Scripts/FaceUpdate.cs
+++ Assets/UnityChan/Scripts/FaceUpdate.cs
@@ -36,9 +36,7 @@ namespace UnityChan
 		void Update ()
 		{
 
-			if (Input.GetMouseButton (0)) {
-				current = 1;
-			} else if (!isKeepFace) {
+			if (!isKeepFace) {
 				current = Mathf.Lerp (current, 0, delayWeight);
 			}
 			anim.SetLayerWeight (1, current);
