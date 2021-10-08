ebbd5b9fd29bb31d593cb3b4f952810baa6bed84
chobitsfan <chobits@itri.org.tw>
2021-10-08 16:39:16 +0800

    add game timer

--- Assets/GameWorld.cs
+++ Assets/GameWorld.cs
@@ -8,13 +8,16 @@ public class GameWorld : MonoBehaviour
     //public DroneAction EmeryDrone;
     //public FPV_CAM FpvCam;
     public UnityEngine.UI.Text HudText;
+    public UnityEngine.UI.Text TimeText;
     public VirtualAction MyVirtualAction;
     public VirtualAction EmeryVirtualAction;
 
-    public bool GameOver => _gameOver;
+    public bool IsGameOver => _gameOver;
 
     float hudTextCd = 0;
     bool _gameOver = false;
+    float remainTime = 3 * 60;
+    bool _gameStarted = false;
 
     public void ShowHudInfo(string text)
     {
@@ -28,15 +31,39 @@ public class GameWorld : MonoBehaviour
         _gameOver = false;
         MyVirtualAction.ResetHP();
         EmeryVirtualAction.ResetHP();
+        remainTime = 3 * 60;
+        _gameStarted = false;
     }
 
     public void GameStart()
+    {
+        ResetGame();
+        _gameStarted = true;
+    }
+
+    void GameOver()
     {
 
     }
 
     private void Update()
     {
+        if (!_gameOver && _gameStarted)
+        {
+            if (remainTime > 0)
+            {
+                remainTime -= Time.deltaTime;
+                int mm = (int)(remainTime / 60);
+                int ss = (int)remainTime % 60;
+                int ms = (int)((remainTime - (int)remainTime) * 100);
+                TimeText.text = mm.ToString("D2") + ":" + ss.ToString("D2") + "." + ms.ToString("D2");
+            }
+            else
+            {
+                _gameOver = true;
+                TimeText.text = "Game Over";
+            }
+        }
         if (hudTextCd > 0)
         {
             hudTextCd -= Time.deltaTime;
@@ -52,6 +79,10 @@ public class GameWorld : MonoBehaviour
         else if (Keyboard.current.rKey.wasPressedThisFrame)
         {
             ResetGame();
+        }
+        else if (Keyboard.current.sKey.wasPressedThisFrame)
+        {
+            GameStart();
         }
         if (!_gameOver)
         {
