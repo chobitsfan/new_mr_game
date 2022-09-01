8bbe396e0b33b8fc13a877ea6703a7328572ec3b
chobits <chobits@itri.org.tw>
2022-09-01 17:00:19 +0800

    use gstreamer

---
+++ Assets/GStreamerUnity/Components/CustomPipelinePlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(GstCustomTexture))]
+public class CustomPipelinePlayer : BaseVideoPlayer {
+
+	// Use this for initialization
+	protected override string _GetPipeline()
+	{
+		string P = "rtspsrc location=" + MyGameSetting.FpvUrl + " latency=0 ! queue ! rtph264depay ! avdec_h264 ! video/x-raw,format=I420 ! videoconvert ! appsink name=videoSink";
+
+		return P;
+	}
+}
